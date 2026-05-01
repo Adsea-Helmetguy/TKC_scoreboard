c3a7f916c8be1a8b9ee2594ed06168346906c284
mlow <marcus.low@ailantis.ai>
2026-05-02 00:05:16 +0800

    updated code

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/TKC_scoreboard/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
